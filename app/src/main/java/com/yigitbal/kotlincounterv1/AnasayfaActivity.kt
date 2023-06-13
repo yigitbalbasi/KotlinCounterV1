9b5421cfd31977d977233344c7b53fa8c0f2ae67
Yigit Balbasi <yigitbalbasi@gmail.com>
2023-06-13 17:12:38 +0300

    first_commit

---
+++ app/src/main/java/com/yigitbal/kotlincounterv1/AnasayfaActivity.kt
@@ -0,0 +1,156 @@
+package com.yigitbal.kotlincounterv1
+
+
+import android.app.AlertDialog
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
+class AnasayfaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anasayfa)
+
+        val kalanMiktarEditText = findViewById<TextView>(R.id.kalanMiktarText)
+        val okunanMiktarEditText = findViewById<TextView>(R.id.okunanMiktarText)
+        val okunanSureEditText = findViewById<TextView>(R.id.okunanSureTextView)
+        val builder = AlertDialog.Builder(this)
+
+       // final MediaPlayer mediaPlayer =
+
+
+        val gelenSayi = intent.getStringExtra("Sayi")
+        println(gelenSayi)
+        var kalanSayi: Int
+        kalanSayi = gelenSayi!!.toInt()
+        var okunanSayi = 0
+
+        kalanMiktarEditText.text = "Kalan: $kalanSayi"
+        okunanMiktarEditText.text = "0"
+
+        val okunacakSure = intent.getStringExtra("Sure")
+        if (okunacakSure == "Sure seçin") {
+            okunanSureEditText.visibility = View.INVISIBLE
+            //görünmez yap
+        } else {
+            okunanSureEditText.text = okunacakSure
+        }
+
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        val buttonGeri = findViewById<Button>(R.id.buttonGeri)
+        buttonGeri.setOnClickListener {
+            builder.setTitle("Geri dönmek istediğinize emin misiniz??")
+
+            builder.setPositiveButton("Evet") { dialog, which ->
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+                Toast.makeText(this, "Geri dönüldü", Toast.LENGTH_LONG).show()
+            }
+
+            builder.setNegativeButton("Hayır") { dialog, which ->
+                println("işlem_iptal")
+            }
+            builder.show()
+
+        }
+
+        val buttonArttir = findViewById<Button>(R.id.buttonArttir)
+        buttonArttir.setOnClickListener {
+            if (kalanSayi != 0) {
+                kalanSayi = kalanSayi - 1
+                okunanSayi = okunanSayi + 1
+                if (kalanSayi == 0) {
+                    Toast.makeText(this, "Bitti", Toast.LENGTH_LONG).show()
+                    it.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+                    val v = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        v.vibrate(
+                            VibrationEffect.createOneShot(
+                                500,
+                                VibrationEffect.DEFAULT_AMPLITUDE
+                            )
+                        )
+                        Log.e("titresim", "uzun")
+                        builder.setTitle("Bitti. Farklı zikir çekmek ister misiniz??")
+
+                        builder.setPositiveButton("Evet") { dialog, which ->
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+
+                        builder.setNegativeButton("Hayır") { dialog, which ->
+                            ActivityCompat.finishAffinity(this)
+                        }
+                        builder.show()
+                    } else {
+                        v.vibrate(500)
+                        Log.e("titresim", "kisa")
+                    }
+                }
+            }
+            kalanMiktarEditText.text = "Kalan: $kalanSayi"
+            okunanMiktarEditText.text = "$okunanSayi"
+
+        }
+
+        val buttonAzalt = findViewById<Button>(R.id.buttonAzalt)
+        buttonAzalt.setOnClickListener {
+            if (kalanSayi != 0) {
+                kalanSayi = kalanSayi + 1
+                okunanSayi = okunanSayi - 1
+            } else {
+                kalanSayi = kalanSayi + 1
+                okunanSayi = okunanSayi - 1
+
+
+            }
+            kalanMiktarEditText.text = "Kalan: $kalanSayi"
+            okunanMiktarEditText.text = "$okunanSayi"
+
+        }
+
+        val buttonReset = findViewById<Button>(R.id.buttonReset)
+        buttonReset.setOnClickListener {
+            builder.setTitle("Sıfırlamak istediğinize emin misiniz??")
+
+            builder.setPositiveButton("Evet") { dialog, which ->
+                kalanSayi = gelenSayi.toInt()
+                okunanSayi = 0
+                kalanMiktarEditText.text = "Kalan: $kalanSayi"
+                okunanMiktarEditText.text = "$okunanSayi"
+                Toast.makeText(this, "Sıfırlandı", Toast.LENGTH_LONG).show()
+            }
+
+            builder.setNegativeButton("Hayır") { dialog, which ->
+                Toast.makeText(
+                    this,
+                    "Hayır", Toast.LENGTH_SHORT
+                ).show()
+            }
+            builder.show()
+
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/yigitbal/kotlincounterv1/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.yigitbal.kotlincounterv1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var spinner: Spinner
+    private lateinit var editText: EditText
+    private lateinit var sayi: String
+    private lateinit var okunacakSure: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lateinit var editText: EditText
+        lateinit var sayi: String
+        editText = findViewById(R.id.numberInputText)
+
+
+
+        val hashMap: HashMap<Int, String> = HashMap() //define empty hashmap
+        hashMap[0] = "Sure seçin"
+        hashMap[1] = "Ayetel Kürsi"
+        hashMap[2] = "Salavat"
+        hashMap[3] = "Fatiha"
+        hashMap[4] = "Felak"
+        hashMap[5] = "Fetih"
+        hashMap[6] = "Fil"
+        hashMap[7] = "Kafirun"
+        hashMap[8] = "Kevser"
+        hashMap[9] = "Kureyş"
+        hashMap[10] = "Maun"
+        hashMap[11] = "İhlas"
+        hashMap[12] = "Nas"
+        hashMap[13] = "Nasr"
+        hashMap[14] = "Tebbet"
+
+        for (key in hashMap.keys) {
+            println("Element at key $key = ${hashMap[key]}")
+        }
+
+        spinner = findViewById(R.id.spinner)
+        val lessonsList: MutableList<String> =
+            mutableListOf(
+                "Sure seçin",
+                "Ayetel Kürsi",
+                "Salavat",
+                "Fatiha",
+                "Felak",
+                "Fetih",
+                "Fil",
+                "Kafirun",
+                "Kevser",
+                "Kureyş",
+                "Maun",
+                "İhlas",
+                "Nas",
+                "Nasr",
+                "Tebbet",
+            )
+
+
+        val adapter =
+            ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, lessonsList)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+        spinner.adapter = adapter
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                okunacakSure = hashMap[p2].toString()
+
+                Toast.makeText(this@MainActivity, okunacakSure, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Toast.makeText(this@MainActivity, "Herhangi bir sure seçilmedi", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        val buttonClick = findViewById<Button>(R.id.buttonKaydet)
+
+        buttonClick.setOnClickListener {
+            sayi = editText.text.toString()
+            if (sayi.isEmpty()) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Lütfen bir sayı girin",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+            } else {
+                if (sayi == "0") {
+                    Toast.makeText(this, "Lütfen sıfırdan farklı bir sayı girin", Toast.LENGTH_LONG)
+                        .show()
+                } else {
+                    val intent = Intent(this, AnasayfaActivity::class.java)
+
+                    intent.putExtra("Sayi", sayi)
+                    intent.putExtra("Sure", okunacakSure)
+                    startActivity(intent)
+                }
+            }
+
+
+        }
+
+
+    }
+
+
+}
